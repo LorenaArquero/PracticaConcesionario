cdc5ec5202472e46df119da15d1775d650ab8bad
jce94 <jcarre00@estudiantes.unileon.es>
2018-05-06 23:43:11 +0200

    Proovedores (Copia de seguridad) - No implementada al 100%
    Posibles mejores, editar estilo de la página y posibilidad de confirmación de pedidos.

---
+++ Proveedores/proveedores.php
@@ -0,0 +1,83 @@
+
+<html5>
+    <head>
+        <title>Page Proveedor</title>
+        
+    </head>
+
+    <body>
+    <br><br><br>
+        <div>
+        <form enctype="multipart/form-data" action="upload.php" method="POST">
+            <!-- MAX_FILE_SIZE debe preceder al campo de entrada del fichero -->
+            <input type="hidden" name="MAX_FILE_SIZE" value="30000" />
+            <!-- El nombre del elemento de entrada determina el nombre en el array $_FILES -->
+            Enviar este fichero: <input name="fichero_usuario" type="file" />
+            <input type="submit" value="Enviar fichero" />
+        </form>
+            <p>
+            <?php
+                        include("Conecxion.php");
+                        $Con = new  conecxion();
+                        
+                        /*Operaciones sobre la tabla de Productos*/
+                            
+                            //$Con->deleteAllElementsFromTableProductos();
+                            //$Con->insertarDatosTablaProductos();
+                        
+                        /*Operaciones sobre la tabla de Pedidos*/
+                            
+                            //$Con->deleteAllElementsFromTablePedidos();
+                            //$Con->insertarDatosTablaPedidos();
+                    ?>
+            <fieldset>
+                <legend>Opciones</legend>
+                
+                    
+                        
+                <form method="post">
+                        <input type="radio" name="gender" <?php if (isset($gender) && $gender=="Ambos") echo "checked";?> value="Ambos (confirmados y no confirmados)">Ambos (confirmados y no confirmados)    
+                        <input type="radio" name="gender" <?php if (isset($gender) && $gender=="Confirmado") echo "checked";?> value="Confirmados">Confirmados
+                        <input type="radio" name="gender" <?php if (isset($gender) && $gender=="No Confirmado") echo "checked";?> value="No Confirmados">No confirmados
+                        <br>
+
+                    <?php $gender = ""?>
+                    <input type="submit" name="submit" value="Submit - Ver"> 
+                    <?php
+                            if (empty($_POST["gender"])) {
+                                $genderErr = "Marque alguna opción.";
+                                echo $genderErr;
+                            } else {
+                                $gender = $_POST["gender"];
+                                echo "Ha seleccionado => " .$gender;   
+                            }
+                        ?>
+                   
+                </form>
+            </fieldset>
+            <br><br><br><br>
+            <fieldset>
+                
+                <legend>Datos: TABLA PEDIDOS <?php echo $gender?></legend>
+                <div>
+                   <?php
+                        if(strcmp($gender, "Ambos (confirmados y no confirmados)")==0 ){
+                            $Con->recuperarDatosProductos();
+                        }else{ 
+                            if(strcmp($gender, "Confirmados")==0){
+                                $Con->recuperarDatosProductosConfirmados();
+                           
+                            }else{
+                                if(strcmp($gender, "No Confirmados")==0){
+                                    $Con->recuperarDatosProductosNoConfirmados();
+                                }  
+                            }
+                        }                      
+                   ?>
+                </div>
+            </fieldset>  
+        </div>
+
+        
+    </body>
+</html5>
---
+++ Proveedores/upload.php
@@ -0,0 +1,57 @@
+<?php
+
+echo "<link rel='stylesheet' type='text/css' href='styleProveedores1.css' />";
+
+$archivo = $_FILES['fichero_usuario']['name'];
+
+$fichero_subido = ($archivo);
+
+echo '<pre>';
+    if (move_uploaded_file($_FILES['fichero_usuario']['tmp_name'], $fichero_subido)) {
+
+        /*Saber si el archivo es de tipo XML.*/
+        if(strpos($archivo, '.xml')!==false){
+            
+            echo "El fichero es válido y se subió con éxito.\n";
+            echo "Nombre del fichero: " . $_FILES['fichero_usuario']['name'] . "\n";
+            
+            /*Apertura del ficher XML*/
+            if (file_exists($archivo)){
+                
+                    if(!$xml = simplexml_load_file($archivo)){
+                        echo "No se ha podido cargar el archivo";
+                    } else {
+                        echo "<table id='productos_table' border='2' width='500'>\n";
+                        
+                        echo "
+                                <tr>
+                                    <th>Id</th>
+                                    <th>Proveedor</th>
+                                    <th>Nombre</th>
+                                    <th>Cantidad</th>
+                                    <th>Descatalogado</th>
+                                </tr>";
+                        foreach($xml as $one){
+                                echo "<tr>";
+                                echo "<td>$one->id</td>";
+                                echo "<td>$one->proveedor</td>";
+                                echo "<td>$one->nombre</td>";
+                                echo "<td>$one->cantidad</td>";
+                                echo "<td>$one->descatalogado</td>";
+                                echo "</tr>";
+                        }
+                        echo "</table>\n";
+                    }
+            } else {
+                $error_message = "Error abriendo " . $archivo; 
+                exit($error_message);
+            }
+        }else{
+            echo "ERROR: El fichero que intenta abrir no es de tipo XML. Vuelva a intentarlo de nuevo.";
+        }
+    } else {
+        echo "¡Posible ataque de subida de ficheros!\n";
+    }
+
+
+?>
