c6acd3ca0f8dce3af0b1ae2e3e90b4e04ba331fd
Pablo Gutiérrez <pgutic03@estudiantes.unileon.es>
2018-05-10 14:17:23 +0200

    Añadido CSS en Pedido y ModificarPedido
    + Añadido CSS a modificar_pedido.php
    + Tabulación del código.
    + Modificaciones menores.
    + Añadido CSS a pedido.php
    + Tabulación del código.
    + Modificaciones menores.
    + Modificado CSS styleConcesionario.css

--- concesionario/pedido.php
+++ concesionario/pedido.php
@@ -1,60 +1,47 @@
  <!DOCTYPE html>
 <html>
-<head>
-    <title>Practica PHP</title>
-    <meta charset="utf-8"/>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script type="text/javascript" src="../scripts/scriptPedidos.js"></script>
-    <link rel = "stylesheet" type = "text/css" href = "../style.css" >		
-</head>
-<body>
+	<head>
+		<title>Nuevo pedido</title>
+		<meta charset="utf-8"/>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		<script type="text/javascript" src="../scripts/scriptPedidos.js"></script>
+		<link rel = "stylesheet" type = "text/css" href = "./styleConcesionario.css" >		
+	</head>
+	<body>
 
-    <?php
+	    <?php
             include("../db_connect/db_connect.php");
             $IDuser = $_GET['IDuser'];
             $sacarConcesionario = mysqli_query($connection, "SELECT username FROM usuario WHERE id ='".$IDuser."'");
     
             while($linea = mysqli_fetch_array($sacarConcesionario)){
-
                 $concesionario =$linea["username"];
-
             }
-    
-    ?>
-    <div id="contenedor">
-    <form action="./rellenar_productos.php" method="POST" id="form" name="formulario">
+    	?>
+    	<div>
+			<fieldset id="contenedor">
+				<legend id="legend">Nuevo pedido</legend>
+				<input type="button" class="button" value="Añadir otro producto" onclick="agregarNuevo()"/>
+				<?php
+		            echo "<input type='button'class='button' value='Hacer pedido' onclick='guardarDatos(".$IDuser.")'/>";
+	        	?>
 
-        <select name="proveedores" id="proveedores">
-            <option value="0" disable selected>Seleccione un proveedor</option>
-            
-        <?php 
-            $sql = mysqli_query($connection, "SELECT username FROM usuario WHERE tipo=\"proveedor\"");
-            while($linea = mysqli_fetch_array($sql)){
-
-                echo "<option value=".$linea[0].">".$linea[0]."</option>";
-
-            }
-        ?>
-        </select>
-        
-            
-        <select id="productos" name="productos">
-            <option value="0" selected>Seleccione un producto</option>
-        </select>
-        
-        
-        <input type="number" name="cantidad" value="0" min="0">
-        
-    </form>
-    </div>
-    
-    <input type="button" value="Añadir" onclick="agregarNuevo()"/>
-	<?php
-            echo "<input type='button' value='hacer pedido' onclick='guardarDatos(".$IDuser.")'/>";
-            
-	?>
-  
-    
-
-</body>
+	    		<form action="./rellenar_productos.php" method="POST" id="form" name="formulario">
+					<select name="proveedores" id="proveedores">
+	            		<option value="0" disable selected>Seleccione un proveedor</option>
+	                	<?php 
+			            	$sql = mysqli_query($connection, "SELECT username FROM usuario WHERE tipo=\"proveedor\"");
+							while ($linea = mysqli_fetch_array($sql)) {
+	                			echo "<option value=".$linea[0].">".$linea[0]."</option>";
+	            			}
+	        			?>
+	        		</select>
+					<select id="productos" name="productos">
+	            		<option value="0" selected>Seleccione un producto</option>
+	        		</select>
+	        		<input type="number" name="cantidad" value="0" min="0">
+	    		</form>
+			</fieldset>
+		</div>
+	</body>
 </html> 
